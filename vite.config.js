2fe0b945eb0c932ae70b509771fe04deed958a66
Haruks-00 <halcamatsu0723@gmail.com>
2025-08-09 22:51:27 +0900

    :recycle: refactor(config): ベースパスをGitHub Pagesのリポジトリ名に合わせて設定

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/ 
 export default defineConfig({
+  // INFO: GitHub Pagesのリポジトリ名に合わせてベースパスを設定
+  base: '/MS_DB_UI/', 
   plugins: [
     vue(),
   ],
